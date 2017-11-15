0ef95308d3f9f436a8d22421c1573d475cba78fc
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-11-15 12:04:03 -0500

    Got it to work

--- Learn_Java_The_Hard_Way(RecursiveBinarySearch)/src/BinarySearch.java
+++ Learn_Java_The_Hard_Way(RecursiveBinarySearch)/src/BinarySearch.java
@@ -3,18 +3,24 @@ public class BinarySearch {
 
 	public static void main(String[]args)
 	{
-		int[] test1 = {1,2,3,4,5,6,7,8,9,10};
-		int[] test2 = {0,4,5,6,12,15,17,23,29};
+		int[] test1 = {0,1,2,3,4,5,6,7,8,9};
+		int[] test2 = {0,1,2,3,4,5,6,7,8};
 		
-		System.out.println(binarySearch(test1,3,0,test1.length)); // 2
-		System.out.println(binarySearch(test2,23,0,test2.length)); // 7
-		System.out.println(binarySearch(test1,1,0,test1.length)); // 0
-		System.out.println(binarySearch(test2,25,0,test2.length)); //-1 this one may cause an error
-		    													  //What's the error message? How can we fix it?
+		for(int i = -1;i<test1.length+1;i++) 
+		{
+			System.out.print(binarySearch(test1, i, 0, test1.length));
+		}
+		System.out.println();
+		for(int i = -1;i<test2.length+1;i++)
+		{
+			System.out.print(binarySearch(test2, i ,0, test2.length));
+		}
+		System.out.println();
+		
 	}
 	public static int binarySearch(int[] nums,int query, int first,int last)
 	{
-		if(last>=first)//if still have some array left to search
+		if(first< last)//if still have some array left to search
 		{
 			int index = (first+last)/2;
 			int guess = nums[index];
@@ -24,11 +30,11 @@ public class BinarySearch {
 			}
 			if(guess>query)
 			{
-				return binarySearch(nums,query,first-1,index);
+				return binarySearch(nums,query,first,index);
 				
 			}
-			if(last<=nums.length)
-			return binarySearch(nums,query,index,last+1);
+			
+			return binarySearch(nums,query,index+1,last);
 			
 		}
 		return -1;
